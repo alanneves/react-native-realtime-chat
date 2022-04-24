2a88663f1979d09af3449697f38184203e5f2584
Alan Neves <alanrenoneves@gmail.com>
2022-04-24 15:40:39 -0300

    feat: add backend

--- user/App.js
+++ user/App.js
@@ -1,10 +1,18 @@
+import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import RootNavigator from './src/navigation';
+import { Amplify } from 'aws-amplify'
+import config from './src/aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
+Amplify.configure(config)
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
+      <StatusBar style="light" />
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App)
